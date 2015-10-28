dcc091da531f652f2df9c1c296cfe1f8dc24287f
Xin Zhang <xzhang@pivotal.io>
2015-10-28 04:02:44 -0700

    CountPages: count number of pages in pdf under given folder.

---
+++ CountPages/CountPages/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CountPages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.beginBox = new System.Windows.Forms.TextBox();
+            this.endBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.folderButton = new System.Windows.Forms.Button();
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.pagesLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "开始病历号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "结束病历号";
+            // 
+            // beginBox
+            // 
+            this.beginBox.Location = new System.Drawing.Point(96, 44);
+            this.beginBox.Name = "beginBox";
+            this.beginBox.Size = new System.Drawing.Size(100, 20);
+            this.beginBox.TabIndex = 3;
+            this.beginBox.Text = "0";
+            this.beginBox.LostFocus += new System.EventHandler(this.beginBox_LostFocus);
+            // 
+            // endBox
+            // 
+            this.endBox.Location = new System.Drawing.Point(340, 46);
+            this.endBox.Name = "endBox";
+            this.endBox.Size = new System.Drawing.Size(100, 20);
+            this.endBox.TabIndex = 5;
+            this.endBox.Text = "9999999";
+            this.endBox.LostFocus += new System.EventHandler(this.endBox_LostFocus);
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(458, 46);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 6;
+            this.runButton.Text = "运行";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(15, 114);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(518, 368);
+            this.logBox.TabIndex = 7;
+            // 
+            // folderButton
+            // 
+            this.folderButton.Location = new System.Drawing.Point(15, 13);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(75, 23);
+            this.folderButton.TabIndex = 0;
+            this.folderButton.Text = "选择目录";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBox
+            // 
+            this.folderBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::CountPages.Properties.Settings.Default, "LastFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.folderBox.Location = new System.Drawing.Point(96, 15);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.Size = new System.Drawing.Size(436, 20);
+            this.folderBox.TabIndex = 1;
+            this.folderBox.Text = global::CountPages.Properties.Settings.Default.LastFolder;
+            this.folderBox.LostFocus += new System.EventHandler(this.folderBox_LostFocus);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "总页数";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "总文件数";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(93, 82);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(13, 13);
+            this.countLabel.TabIndex = 10;
+            this.countLabel.Text = "0";
+            // 
+            // pagesLabel
+            // 
+            this.pagesLabel.AutoSize = true;
+            this.pagesLabel.Location = new System.Drawing.Point(340, 82);
+            this.pagesLabel.Name = "pagesLabel";
+            this.pagesLabel.Size = new System.Drawing.Size(13, 13);
+            this.pagesLabel.TabIndex = 11;
+            this.pagesLabel.Text = "0";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(458, 77);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "清空";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 504);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.pagesLabel);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.folderBox);
+            this.Controls.Add(this.folderButton);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.endBox);
+            this.Controls.Add(this.beginBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "扫描页数记录器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox beginBox;
+        private System.Windows.Forms.TextBox endBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label pagesLabel;
+
+        private int _begin;
+        private int _end;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ CountPages/CountPages/Form1.cs
@@ -0,0 +1,324 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace CountPages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this._begin = Int32.Parse(beginBox.Text);
+            this._end = Int32.Parse(endBox.Text);
+            this.runButton.Enabled = validateRun();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            if (this.runThread != null)
+            {
+                if (this.runThread.IsAlive)
+                {
+                    this.runThread.Abort();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+
+            if(folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderBox.Text = folderBrowserDialog1.SelectedPath;
+                global::CountPages.Properties.Settings.Default.LastFolder = folderBox.Text;
+                global::CountPages.Properties.Settings.Default.Save();
+            }
+        }
+
+        private void folderBox_LostFocus(object sender, System.EventArgs e)
+        {
+            global::CountPages.Properties.Settings.Default.LastFolder = folderBox.Text;
+            global::CountPages.Properties.Settings.Default.Save();
+            runButton.Enabled = validateRun();
+        }
+
+        private void beginBox_LostFocus(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(beginBox.Text, out _begin))
+            {
+                logBox.AppendText("开始病历号必须是数字!\n");
+                _begin = -1;
+            }
+
+            runButton.Enabled = validateRun();
+        }
+
+        private bool validateRun()
+        {
+            return (
+                !String.IsNullOrEmpty(beginBox.Text)
+                && !String.IsNullOrEmpty(endBox.Text)
+                && !String.IsNullOrEmpty(folderBox.Text)
+                && _begin > -1
+                && _end > -1);
+        }
+
+        private void endBox_LostFocus(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(endBox.Text, out _end))
+            {
+                logBox.AppendText("结束病历号必须是数字！\n");
+                _end = -1;
+            }
+
+            if (_end < _begin)
+            {
+                logBox.AppendText("结束病历号必须大于开始病历号！\n");
+            }
+
+            runButton.Enabled = validateRun();
+        }
+
+        Thread runThread;
+        bool cancel = false;
+
+        private void runButton_Click(object sender, EventArgs ea)
+        {
+            if (runThread != null)
+            {
+                if (!cancel)
+                {
+                    cancel = true;
+                    runButton.Text = "重置";
+                }
+                else if (!runThread.IsAlive)
+                {
+                    runThread = null;
+                    runButton.Text = "运行";
+                    cancel = false;
+                }
+            }
+            else
+            {
+                cancel = false;
+                runThread = new Thread(() => { run(); });
+                runThread.Start();
+                runButton.Text = "中止";
+            }
+        }
+
+        delegate void logTextCallback(string text);
+
+        private void logText(string text)
+        {
+            if (this.logBox.InvokeRequired)
+            {
+                logTextCallback d = new logTextCallback(logText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.logBox.AppendText(text);
+            }
+        }
+
+        delegate void setCountCallback(int count);
+
+        private void setCount(int count)
+        {
+            if (this.countLabel.InvokeRequired)
+            {
+                setCountCallback d = new setCountCallback(setCount);
+                this.Invoke(d, new object[] { count });
+            }
+            else
+            {
+                this.countLabel.Text = count.ToString();
+            }
+        }
+
+        delegate void setPagesCallback(int pages);
+
+        private void setPages(int pages)
+        {
+            if (this.pagesLabel.InvokeRequired)
+            {
+                setPagesCallback d = new setPagesCallback(setPages);
+                this.Invoke(d, new object[] { pages });
+            }
+            else
+            {
+                this.pagesLabel.Text = pages.ToString();
+            }
+        }
+
+        private void run()
+        { 
+            int count = 0;
+            int pages = 0;
+
+            if (!Directory.Exists(folderBox.Text))
+            {
+                logText(string.Format("文件目录:{0} 不存在！\n", folderBox.Text));
+                return;
+            }
+
+            // figure out current month
+            DateTime current = DateTime.Now;
+
+            // current week # of files
+            // current week # of pages
+            int curWeekFiles = 0;
+            int curWeekPages = 0;
+
+            // last week # of files
+            // last week # of pages
+            int lastWeekFiles = 0;
+            int lastWeekPages = 0;
+
+            // current month # of files
+            // current month # of pages
+            int curMonthFiles = 0;
+            int curMonthPages = 0;
+
+            // last month # of files
+            // last month # of pages
+            int lastMonthFiles = 0;
+            int lastMonthPages = 0;
+
+            Queue<string> directories = new Queue<string>();
+            directories.Enqueue(folderBox.Text);
+
+            while (directories.Count > 0)
+            {
+                if (cancel)
+                {
+                    break;
+                }
+
+                string currentDirectory = directories.Dequeue();
+
+                logText(string.Format("正在统计{0}...\n", currentDirectory));
+
+                foreach (string directory in Directory.GetDirectories(currentDirectory, "*"))
+                {
+                    directories.Enqueue(directory);
+                }
+
+                foreach (string fileName in Directory.GetFiles(currentDirectory, "*.pdf"))
+                {
+                    if (cancel)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        string name = Path.GetFileNameWithoutExtension(fileName);
+                        int num = Int32.Parse(name);
+
+                        if (num >= _begin && num <= _end)
+                        {
+                            count++;
+
+                            FileInfo fi = new FileInfo(fileName);
+                            DateTime creation = fi.LastWriteTime;
+                            int monthDuration = getMonths(creation, current);
+                            int weekDuration = getWeeks(creation, current);
+
+                            if (monthDuration == 0)
+                            {
+                                curMonthFiles++;
+                            }
+                            else if (monthDuration == 1)
+                            {
+                                lastMonthFiles++;
+                            }
+
+                            if (weekDuration == 0)
+                            {
+                                curWeekFiles++;
+                            }
+                            else if (weekDuration == 1)
+                            {
+                                lastWeekFiles++;
+                            }
+
+                            using (PdfReader reader = new PdfReader(fileName))
+                            {
+                                logText(string.Format("{0}: {1} 页\n", Path.GetFileName(fileName), reader.NumberOfPages));
+
+                                pages += reader.NumberOfPages;
+
+                                if (monthDuration == 0)
+                                {
+                                    curMonthPages += reader.NumberOfPages;
+                                }
+                                else if (monthDuration == 1)
+                                {
+                                    lastMonthPages += reader.NumberOfPages;
+                                }
+
+                                if (weekDuration == 0)
+                                {
+                                    curWeekPages += reader.NumberOfPages;
+                                }
+                                else if (weekDuration == 1)
+                                {
+                                    lastWeekPages += reader.NumberOfPages;
+                                }
+
+                                reader.Close();
+                            }
+
+                            setCount(count);
+                            setPages(pages);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        System.Console.Error.WriteLine(e.Message);
+                        System.Console.Error.WriteLine(e.StackTrace);
+                    }
+                }
+            }
+
+            logText(string.Format("总文件数: {0} 总页数: {1}\n", count, pages));
+            logText(string.Format("本周文件数： {0} 页数： {1}\n", curWeekFiles, curWeekPages));
+            logText(string.Format("上周文件数： {0} 页数： {1}\n", lastWeekFiles, lastWeekPages));
+            logText(string.Format("本月文件数： {0} 页数： {1}\n", curMonthFiles, curMonthPages));
+            logText(string.Format("上月文件数： {0} 页数： {1}\n", lastMonthFiles, lastMonthPages));
+        }
+
+        private int getWeeksInYear(DateTime current)
+        {
+            CultureInfo ci = CultureInfo.CurrentCulture;
+            return ci.Calendar.GetWeekOfYear(current, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+        }
+
+        private int getMonths(DateTime start, DateTime end)
+        {
+            return (end.Year - start.Year) * 12 + end.Month - start.Month;
+        }
+
+        private int getWeeks(DateTime start, DateTime end)
+        {
+            return (end.Year - start.Year) * 52 + getWeeksInYear(end) - getWeeksInYear(start);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            logBox.Clear();
+        }
+    }
+}
